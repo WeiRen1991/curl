2f85b70ff6bbf974069d81cf5a42d566de06b7bd
Daniel Stenberg <daniel@haxx.se>
2020-11-28 16:42:52 +0100

    openssl: use OPENSSL_init_ssl() with >= 1.0.0
    Reported-by: Kovalkov Dmitrii and Per Nilsson
    Fixes #6254
    Fixes #6256
    Closes #

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -1111,6 +1111,11 @@ static int x509_name_oneline(X509_NAME *a, char *buf, size_t size)
 #endif
 }
 
+#ifdef CURL_DISABLE_OPENSSL_AUTO_LOAD_CONFIG
+#define INITFLAGS OPENSSL_INIT_NO_LOAD_CONFIG
+#else
+#define INITFLAGS OPENSSL_INIT_LOAD_CONFIG
+#endif
 /**
  * Global SSL init
  *
@@ -1119,6 +1124,10 @@ static int x509_name_oneline(X509_NAME *a, char *buf, size_t size)
  */
 static int Curl_ossl_init(void)
 {
+#if (OPENSSL_VERSION_NUMBER >= 0x10100000L) &&  \
+  !defined(LIBRESSL_VERSION_NUMBER)
+  OPENSSL_init_ssl(INITFLAGS, NULL);
+#else
   OPENSSL_load_builtin_modules();
 
 #ifdef USE_OPENSSL_ENGINE
@@ -1137,10 +1146,6 @@ static int Curl_ossl_init(void)
                          CONF_MFLAGS_IGNORE_MISSING_FILE);
 #endif
 
-#if (OPENSSL_VERSION_NUMBER >= 0x10100000L) && \
-    !defined(LIBRESSL_VERSION_NUMBER)
-  /* OpenSSL 1.1.0+ takes care of initialization itself */
-#else
   /* Lets get nice error messages */
   SSL_load_error_strings();
 
